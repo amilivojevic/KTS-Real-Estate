0bfd768eeba681185a74d54911e5a1a3743b0eec
amilivojevic <aleksandra.milivojevic.94@gmail.com>
2017-05-09 13:19:03 +0200

    Project setup

---
+++ src/main/java/kts/project/KtsprojectApplication.java
@@ -0,0 +1,12 @@
+package kts.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtsprojectApplication.class, args);
+	}
+}
