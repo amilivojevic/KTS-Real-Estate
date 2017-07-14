f7374f2a6defee9645e559464692f30192f28358
NinaSimic <ninasimicns@gmail.com>
2017-07-14 17:15:14 +0200

    Float and int validations done

--- src/main/webapp/app/realEstate/addNewRealEstate.controller.js
+++ src/main/webapp/app/realEstate/addNewRealEstate.controller.js
@@ -47,6 +47,18 @@
 
             }
 
+
+                function isNumber(evt) {
+                    evt = (evt) ? evt : window.event;
+                    var charCode = (evt.which) ? evt.which : evt.keyCode;
+                    if ( (charCode > 31 && charCode < 48) || charCode > 57) {
+                        return false;
+                    }
+                    return true;
+                }
+
+
+
             console.log("novi korisnik: " + JSON.stringify(vm.newUser) )
 
             $http.post('/api/realEstate/addNewRealEstate', vm.new_real_estate).then(function (response) {
