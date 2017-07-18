2521291a7a2288cee84f3ab935d867c15b1a6750
Sandra <aleksandra.milivojevic.94@gmail.com>
2017-07-18 03:45:56 +0200

    np

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -10,27 +10,31 @@ insert into authority (name) values ('ROLE_VERIFYER');
 
 --password: admin
 --usernmae: admin
+
+--id 1
 INSERT INTO `user`(`id`,`account_number`,`address`,`birth_date`,`city`,`country`,`email`,`image_url`,`name`,`password`,`phone_number`,`role`,`surname`,`username`,`authority_id`)VALUES(1,0000,"Bul. Oslobodjenja 1","01.01.2000.","Novi Sad","Serbia","default@gmail.com","images/img1.jpg","admin default","$2a$10$2nUwyYdXRxhKio6aVrwsVOXlUqSs9XnsIndPiyT.3AphhvZ/UYBta","021 111 111","SYS_ADMIN","admin","admin",1);
+--id 2
 INSERT INTO `user`(`id`,`account_number`,`address`,`birth_date`,`city`,`country`,`email`,`image_url`,`name`,`password`,`phone_number`,`role`,`surname`,`username`,`authority_id`)VALUES(2,0012300,"Trg Majke Jevrosime 11","17.07.2017.","Wroclaw","Poland","nina@gmail.com","images/img2.jpg","pera","$2a$10$2nUwyYdXRxhKio6aVrwsVOXlUqSs9XnsIndPiyT.3AphhvZ/UYBta","06415896325","SYS_ADMIN","peric","admin2",1);
 
 
 --password: ver
 --username: ver
+--id 3
 INSERT INTO `user`(`id`,`account_number`,`address`,`birth_date`,`city`,`country`,`email`,`image_url`,`name`,`password`,`phone_number`,`role`,`surname`,`username`,`authority_id`)VALUES(3,0000,"Bul. Oslobodjenja 1","01.01.2000.","Novi Sad","Serbia","default@gmail.com","images/img1.jpg","verifyer default","$2a$10$SmLd5yKXvmiPdb03xmDoJO.0O/urG3vLsYT5W2MB5UadLCr8V7BoG","021 111 111","VERIFYER","verifyer","ver",3);
 
 -- owner
-
-INSERT INTO `user`(`id`,`account_number`,`address`,`birth_date`,`city`,`country`,`email`,`image_url`,`name`,`password`,`phone_number`,`role`,`surname`,`username`,`authority_id`)VALUES(9,59856,"Jovana Ducica 12","07.07.2010.","Beograd","Serbia","sandra1@gmail.com","images/img2.jpg","sandra","$2a$10$Qt3BU0OfliX1n2wGSE7E6.Evy9JryYxQp5VbW2FohZyAuv/bej1cq","021 333 555","OWNER","Jaimez","g",2);
+--id 9
+INSERT INTO `user`(`id`,`account_number`,`address`,`birth_date`,`city`,`country`,`email`,`image_url`,`name`,`password`,`phone_number`,`role`,`surname`,`username`,`authority_id`)VALUES(9,5985,"Jovana Ducica 12","07.07.2010.","Beograd","Serbia","sandra1@gmail.com","images/img2.jpg","sandra","$2a$10$Qt3BU0OfliX1n2wGSE7E6.Evy9JryYxQp5VbW2FohZyAuv/bej1cq","021 333 555","OWNER","Jaimez","g",2);
+--id 4
 INSERT INTO `user`(`id`,`account_number`,`address`,`birth_date`,`city`,`country`,`email`,`image_url`,`name`,`password`,`phone_number`,`role`,`surname`,`username`,`authority_id`)VALUES(4,3696,"Reja 54","01.01.2000.","Wroclaw","Poland","marko@gmail.com","images/img1.jpg","marko","$2a$10$HeR9A53PD1jmyIJn1IyZOe5o4zu./hc7LZuPV41Ie84prg2JVfo2O","021 111 111","OWNER","Markovic","a",2);
-INSERT INTO `user`(`id`,`account_number`,`address`,`birth_date`,`city`,`country`,`email`,`image_url`,`name`,`password`,`phone_number`,`role`,`surname`,`username`,`authority_id`)VALUES(5,59856,"Jovana Ducica 12","07.07.2010.","Beograd","Serbia","sandra1@gmail.com","images/img2.jpg","sandra","$2a$10$Qt3BU0OfliX1n2wGSE7E6.Evy9JryYxQp5VbW2FohZyAuv/bej1cq","021 333 555","OWNER","Jaimez","g",2);
-
+--id 7
+INSERT INTO `user`(`id`,`account_number`,`address`,`birth_date`,`city`,`country`,`email`,`image_url`,`name`,`password`,`phone_number`,`role`,`surname`,`username`,`authority_id`)VALUES(7,3696,"Reja 54","01.01.2000.","Wroclaw","Poland","marko@gmail.com","images/img1.jpg","marko","$2a$10$HeR9A53PD1jmyIJn1IyZOe5o4zu./hc7LZuPV41Ie84prg2JVfo2O","021 111 111","OWNER","Markovic","a",2);
 
 INSERT INTO `owner`(`id`)VALUES(9);
 INSERT INTO `owner`(`id`)VALUES(4);
-INSERT INTO `owner`(`id`)VALUES(5);
+INSERT INTO `owner`(`id`)VALUES(7);
 
-
-INSERT INTO `company`(`approved`, `fax`,`pib`, `site`, `id` )VALUES(true, "fax259945121", "PIB1", "http\nista", 5);
+INSERT INTO `company`(`approved`, `fax`,`pib`, `site`, `id` )VALUES(true, "fax259945121", "PIB1", "http\nista", 7);
 
 INSERT INTO `private_account_in_company`(`id`, `approved`,`company`, `owner` )VALUES(1, true, 5, 9);
 
